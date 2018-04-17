e8aff6f5413bf3a7ae636976558431203d9a8c4a
David Wilkie <dwilkie@gmail.com>
2018-04-17 19:56:09 +0700

    Switch to somleng.ews1294.info

--- infrastructure/terraform/ews1294.info/variables.tf
+++ infrastructure/terraform/ews1294.info/variables.tf
@@ -6,7 +6,7 @@ locals {
   twilreapi_major_ruby_version = "2.5"
   twilreapi_identifier = "somleng-twilreapi"
   twilreapi_url_host = "https://somleng.ews1294.info"
-  twilreapi_route53_record_name = "twilio"
+  twilreapi_route53_record_name = "somleng"
   twilreapi_db_master_password = "AQICAHh5ylDKuj3jGBOphV/NIPGWxWaKQ5XSe4/KMCjtwW8boQEcnR7BnH68i203YgnzCHi/AAAAeDB2BgkqhkiG9w0BBwagaTBnAgEAMGIGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQM/mMu7y/b3cYmnv+GAgEQgDVZ59Gz995YjX1SZ3YsgHg2i6NwcqQbSwjzOm+g5U98UvaZcodoBDE+ht6Gql7x9mbrE7Fi3g=="
   twilreapi_rails_master_key = "AQICAHh5ylDKuj3jGBOphV/NIPGWxWaKQ5XSe4/KMCjtwW8boQH8yUs4O6M24hSmrBkZMe2jAAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMtJFSalilQVqpDrkrAgEQgDtqyhqv/s2cRHiyWLSIqEn6JkTpN9RzhSkNchtAmwdFbcoLHfGGgfNqPaDEw4JmZEBzWlTMrOMd7ORYaA=="
   # change me when somleng-adhearsion is terraformed
