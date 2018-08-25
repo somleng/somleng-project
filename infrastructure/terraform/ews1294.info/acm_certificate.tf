be64982d453f250d3724eb6f67e17afdcb18e1e5
David Wilkie <dwilkie@gmail.com>
2018-08-25 11:17:49 +0700

    Move terraform configuration for PIN to https://github.com/PIN-Cambodia/somleng-scfm

--- infrastructure/terraform/ews1294.info/acm_certificate.tf
+++
@@ -1,3 +0,0 @@
-data "aws_acm_certificate" "ews1294" {
-  domain = "*.ews1294.info"
-}
--- infrastructure/terraform/ews1294.info/eb_solution_stacks.tf
+++
@@ -1,9 +0,0 @@
-module "twilreapi_eb_solution_stack" {
-  source             = "../modules/eb_solution_stacks"
-  major_ruby_version = "${local.twilreapi_major_ruby_version}"
-}
-
-module "scfm_eb_solution_stack" {
-  source             = "../modules/eb_solution_stacks"
-  major_ruby_version = "${local.scfm_major_ruby_version}"
-}
