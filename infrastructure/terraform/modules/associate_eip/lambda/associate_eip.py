5f5be96c56eb6ca63cb00d9dcc479dbe8c594985
David Wilkie <dwilkie@gmail.com>
2018-10-29 18:30:10 +0700

    Fix syntax error in Lamda function

--- infrastructure/terraform/modules/associate_eip/lambda/associate_eip.py
+++ infrastructure/terraform/modules/associate_eip/lambda/associate_eip.py
@@ -8,12 +8,12 @@ ec2_client = boto3.client('ec2')
 asg_client = boto3.client('autoscaling')
 
 def lambda_handler(event, context):
-	log("Received Event: {}".format(event))
+  log("Received Event: {}".format(event))
 
   if event["detail-type"] != os.environ['EVENT_DETAIL_TYPE']:
-		return
+    return
 
-	log("Processing Event")
+  log("Processing Event")
 
   instance_id = event['detail']['EC2InstanceId']
   LifecycleHookName=event['detail']['LifecycleHookName']
@@ -26,8 +26,8 @@ def lambda_handler(event, context):
 
 def get_in_service_instance_id(asg_group_name):
   response = asg_client.describe_auto_scaling_groups(
-		AutoScalingGroupNames=[asg_group_name]
-	)
+  AutoScalingGroupNames=[asg_group_name]
+  )
 
   in_service_instances = filter(lambda x: x["LifecycleState"] == "InService", response['AutoScalingGroups'][0]['Instances'])
 
@@ -39,7 +39,7 @@ def associate_address(instance_id):
     InstanceId=instance_id
   )
 
-	log("Associated IP Address with: {}. Response: {}".format(instance_id, response))
+  log("Associated IP Address with: {}. Response: {}".format(instance_id, response))
 
 def remove_unassociated_addresses():
   filters = [{'Name': 'domain', 'Values': ['vpc']}]
@@ -60,10 +60,10 @@ def complete_lifecycle_action_success(hookname, groupname, instance_id):
       InstanceId=instance_id,
       LifecycleActionResult='CONTINUE'
     )
-		log("Lifecycle hook CONTINUEd for: {}".format(instance_id))
+    log("Lifecycle hook CONTINUEd for: {}".format(instance_id))
   except botocore.exceptions.ClientError as e:
-	  log("Error completing life cycle hook for instance {}: {}".format(instance_id, e.response['Error']))
-	  log('{"Error": "1"}')
+    log("Error completing life cycle hook for instance {}: {}".format(instance_id, e.response['Error']))
+    log('{"Error": "1"}')
 
 def complete_lifecycle_action_failure(hookname,groupname,instance_id):
   try:
