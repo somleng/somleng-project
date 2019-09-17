c0c236be71c51ccbdf5948797bb09bffbd36057f
David Wilkie <dwilkie@gmail.com>
2019-09-17 13:25:01 +0700

    Change Orange Sierra Leone's IP

--- infrastructure/somleng.org/sg.tf
+++ infrastructure/somleng.org/sg.tf
@@ -14,7 +14,7 @@ resource "aws_security_group_rule" "orange_sierra_leone" {
   from_port   = 5060
   to_port     = 5060
   protocol    = "udp"
-  cidr_blocks = ["41.191.249.52/32"]
+  cidr_blocks = ["197.215.105.30/32"]
   description = "Orange Sierra Leone"
 
   security_group_id = "${module.freeswitch_main.security_group_id}"
