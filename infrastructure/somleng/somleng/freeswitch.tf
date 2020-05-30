2dc0b7499d6f54582a6000f44229fa50e06682d1
David Wilkie <dwilkie@gmail.com>
2020-05-30 13:06:47 +0700

    Update scheduler

--- infrastructure/somleng/somleng/freeswitch.tf
+++ infrastructure/somleng/somleng/freeswitch.tf
@@ -455,7 +455,7 @@ resource "aws_elastic_beanstalk_environment" "freeswitch_webserver" {
 resource "aws_autoscaling_schedule" "freeswitch_restart_autoscale_up" {
   scheduled_action_name  = "freeswitch-restart-autoscale-up"
   autoscaling_group_name = element(aws_elastic_beanstalk_environment.freeswitch_webserver.autoscaling_groups, 0)
-  recurrence = "30 * * * *"
+  recurrence = "0 20 * * *"
   desired_capacity = 2
   min_size = 1
   max_size = 2
