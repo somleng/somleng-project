a49e289d50175eafc79f13101658e843807863e2
David Wilkie <dwilkie@gmail.com>
2020-08-15 17:15:10 +0700

    WIP

--- infrastructure/somleng/core/ecs.tf
+++ infrastructure/somleng/core/ecs.tf
@@ -1,3 +1,4 @@
 resource "aws_ecs_cluster" "somleng" {
   name = "somleng"
+  capacity_providers = ["FARGATE", "FARGATE_SPOT"]
 }
--- infrastructure/somleng/core/route53.tf
+++ infrastructure/somleng/core/route53.tf
@@ -126,6 +126,18 @@ resource "aws_route53_record" "scfm" {
   }
 }
 
+resource "aws_route53_record" "ahn" {
+  zone_id = aws_route53_zone.somleng_org.zone_id
+  name    = "ahn"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.somleng_application.dns_name
+    zone_id                = aws_lb.somleng_application.zone_id
+    evaluate_target_health = true
+  }
+}
+
 resource "aws_route53_record" "freeswitch" {
   zone_id = aws_route53_zone.somleng_org.zone_id
   name    = "freeswitch"
