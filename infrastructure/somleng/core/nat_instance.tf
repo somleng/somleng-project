c2fa15c9017a76151301572f69aa95b8b65a310c
David Wilkie <dwilkie@gmail.com>
2022-03-23 11:16:00 +0700

    NAT instance monitoring

--- infrastructure/somleng/core/nat_instance.tf
+++ infrastructure/somleng/core/nat_instance.tf
@@ -199,6 +199,22 @@ resource "aws_autoscaling_group" "nat_instance" {
   }
 }
 
+
+# Automatically update the SSM agent
+
+# https://docs.aws.amazon.com/systems-manager/latest/userguide/sysman-state-cli.html
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ssm_association
+resource "aws_ssm_association" "update_ssm_agent" {
+  name = "AWS-UpdateSSMAgent"
+
+  targets {
+    key    = "tag:Name"
+    values = [local.nat_instance_name]
+  }
+
+  schedule_expression = "cron(0 19 ? * SAT *)"
+}
+
 # Routes that need to use the NAT Instance
 
 resource "aws_route" "zamtel" {
