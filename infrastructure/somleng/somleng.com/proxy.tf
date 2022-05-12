ac853dd25d0961a2a2974fa76b73e87fd45423df
Samnang Chhun <samnang.chhun@gmail.com>
2022-05-12 16:56:46 +0700

    WIP

---
+++ infrastructure/somleng/somleng.com/proxy.tf
@@ -0,0 +1,23 @@
+module "dashboard_proxy" {
+  source = "github.com/somleng/terraform-aws-cloudfront-reverse-proxy"
+
+  origin_domain_name = "dashboard.somleng.org"
+  domain_name = aws_route53_zone.somleng_com.name
+  host_name = "x-dashboard"
+  zone_id = aws_route53_zone.somleng_com.zone_id
+
+  create_certificate = false
+  certificate_arn = aws_acm_certificate.this.arn
+}
+
+module "api_proxy" {
+  source = "github.com/somleng/terraform-aws-cloudfront-reverse-proxy"
+
+  origin_domain_name = "api.somleng.org"
+  domain_name = aws_route53_zone.somleng_com.name
+  host_name = "x-api"
+  zone_id = aws_route53_zone.somleng_com.zone_id
+
+  create_certificate = false
+  certificate_arn = aws_acm_certificate.this.arn
+}
---
+++ infrastructure/somleng/somleng.com/terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket  = "infrastructure.somleng.org"
+    key     = "somleng.com.tfstate"
+    encrypt = true
+    region  = "ap-southeast-1"
+  }
+
+  required_version = ">= 0.12"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  alias   = "us-east-1"
+}
