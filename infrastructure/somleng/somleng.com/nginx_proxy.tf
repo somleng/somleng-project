e1e79ae5cb681418c64277e8a6f30ff7732b5474
Samnang Chhun <samnang.chhun@gmail.com>
2022-05-18 19:42:00 +0700

    WIP

--- infrastructure/somleng/somleng.com/nginx_proxy.tf
+++ infrastructure/somleng/somleng.com/nginx_proxy.tf
@@ -25,13 +25,10 @@ resource "aws_internet_gateway" "somleng_com" {
   }
 }
 
-resource "aws_default_route_table" "main" {
-  default_route_table_id = aws_vpc.somleng_com.default_route_table_id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.somleng_com.id
-  }
+resource "aws_route" "somleng_com" {
+  route_table_id            = aws_vpc.somleng_com.default_route_table_id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.somleng_com.id
 }
 
 resource "aws_security_group" "somleng_com" {
