4c636ee06e24307ae3e473741363a1183b011241
David Wilkie <dwilkie@gmail.com>
2023-08-23 14:40:37 +0700

    Upgrade NAT instance to use Amazon Linux 2023

--- infrastructure/somleng/core/templates/nat_instance/setup.sh
+++ infrastructure/somleng/core/templates/nat_instance/setup.sh
@@ -1,15 +1,18 @@
 #!/bin/bash -x
 
 yum -y update
-yum -y install iptables-services jq
+yum -y install iptables iptables-services jq
 
 # Enable SSM Access
 
 systemctl enable amazon-ssm-agent
 systemctl start amazon-ssm-agent
 
-EC2_INSTANCE_ID="$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id)"
-AWS_REGION="$(wget -q -O - http://169.254.169.254/latest/meta-data/placement/region)"
+# Get the instance metadata
+
+TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+AWS_REGION="$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/region)"
+INSTANCE_ID="$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id)"
 
 # attach the ENI
 # Retry incase this instance boots before the old one shuts down
@@ -19,7 +22,7 @@ retries=60
 for ((i=0; i<retries; i++)); do
   aws ec2 attach-network-interface \
     --region $AWS_REGION \
-    --instance-id $EC2_INSTANCE_ID \
+    --instance-id $INSTANCE_ID \
     --device-index 1 \
     --network-interface-id "${eni_id}"
 
@@ -36,12 +39,10 @@ describe_alarm_result=$(aws cloudwatch describe-alarms --alarm-names ${cloudwatc
 
 aws cloudwatch put-metric-alarm \
   --cli-input-json "$describe_alarm_result" \
-  --dimensions "Name=InstanceId,Value=$EC2_INSTANCE_ID" \
+  --dimensions "Name=InstanceId,Value=$INSTANCE_ID" \
   --alarm-actions arn:aws:automate:$AWS_REGION:ec2:terminate \
   --region $AWS_REGION
 
 # start SNAT
 systemctl enable snat
 systemctl start snat
-
-
