b8674874b3904b46820a2718001417a6b33e51ee
David Wilkie <dwilkie@gmail.com>
2024-04-07 11:35:25 +0700

    Allow egress traffic to all ports from ALB

--- infrastructure/somleng/core/alb.tf
+++ infrastructure/somleng/core/alb.tf
@@ -23,10 +23,10 @@ resource "aws_security_group_rule" "http_ingress" {
   security_group_id = aws_security_group.somleng_application_load_balancer.id
 }
 
-resource "aws_security_group_rule" "http_egress" {
+resource "aws_security_group_rule" "egress" {
   type        = "egress"
-  from_port   = 80
-  to_port     = 80
+  from_port   = 0
+  to_port     = 65535
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
