a77efc24781196eb2b420386f2dd8535e9339616
David Wilkie <dwilkie@gmail.com>
2024-09-03 17:15:15 +0700

    Add region data

---
+++ infrastructure/somleng/core/ssm.tf
@@ -0,0 +1,41 @@
+resource "aws_ssm_parameter" "somleng_regions_production" {
+  name = "somleng.production.region_data"
+  type = "String"
+  value = jsonencode(
+    [
+      {
+        alias      = "hydrogen",
+        identifier = var.aws_default_region,
+        human_name = "Asia",
+        group_id   = 1,
+      },
+      {
+        alias      = "helium",
+        identifier = var.aws_helium_region,
+        human_name = "North America",
+        group_id   = 2
+      }
+    ]
+  )
+}
+
+resource "aws_ssm_parameter" "somleng_regions_staging" {
+  name = "somleng.staging.region_data"
+  type = "String"
+  value = jsonencode(
+    [
+      {
+        alias      = "hydrogen",
+        identifier = var.aws_default_region,
+        human_name = "Asia",
+        group_id   = 1
+      },
+      {
+        alias      = "helium",
+        identifier = var.aws_helium_region,
+        human_name = "North America",
+        group_id   = 2
+      }
+    ]
+  )
+}
