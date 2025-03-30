1fa07c84ba5fc89613e6b2218ec43b3d0ea475f1
David Wilkie <dwilkie@gmail.com>
2025-03-30 09:53:09 +0700

    Add S3 target groups

--- infrastructure/somleng/modules/region/outputs.tf
+++ infrastructure/somleng/modules/region/outputs.tf
@@ -6,6 +6,10 @@ output "vpc_endpoints" {
   value = module.vpc_endpoints
 }
 
+output "s3_target_group" {
+  value = var.create_s3_vpc_endpoint ? aws_lb_target_group.s3[0] : null
+}
+
 output "public_load_balancer" {
   value = var.create_public_load_balancer ? module.public_load_balancer[0] : null
 }
