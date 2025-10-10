81608df89400d74c5be95f6e4d3be7e7d1bf3ae2
David Wilkie <dwilkie@gmail.com>
2025-10-10 17:23:42 +0700

    Add E2E tests for outbound flow

--- infrastructure/somleng/testing/variables.tf
+++ infrastructure/somleng/testing/variables.tf
@@ -1,3 +1,13 @@
 locals {
-  region = data.terraform_remote_state.core_infrastructure.outputs.hydrogen_region
+  region           = data.terraform_remote_state.core_infrastructure.outputs.hydrogen_region
+  use_nat_instance = lower(var.use_nat_instance) == "y"
+}
+
+variable "use_nat_instance" {
+  description = "Use the NAT instance? y or n"
+  type        = string
+  validation {
+    condition     = can(regex("^[yYnN]$", var.use_nat_instance))
+    error_message = "You must enter 'y' or 'n'."
+  }
 }
