d4932c4c69dd67bc2951b693d5e414cc6737c556
David Wilkie <dwilkie@gmail.com>
2026-03-24 19:11:24 +0700

    Add cross-region sgs

--- infrastructure/somleng/core/db.tf
+++ infrastructure/somleng/core/db.tf
@@ -1,16 +1,16 @@
 module "db" {
+  source              = "../modules/db"
   identifier          = "somleng"
   database_username   = "somleng"
   database_identifier = "somlengv2"
-  source              = "../modules/db"
   region              = module.hydrogen_region
   engine_version      = "17.5"
 }
 
 module "db_staging" {
+  source            = "../modules/db"
   identifier        = "somleng-staging"
   database_username = "somleng"
-  source            = "../modules/db"
   region            = module.hydrogen_region
   min_capacity      = 0
   engine_version    = "17.6"
