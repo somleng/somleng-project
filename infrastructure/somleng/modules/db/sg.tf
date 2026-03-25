c0d35b120b827e2e5f4a4dfb37886e26c961acd5
David Wilkie <dwilkie@gmail.com>
2026-03-25 23:32:20 +0700

    Fix cross zone database security group

--- infrastructure/somleng/modules/db/sg.tf
+++ infrastructure/somleng/modules/db/sg.tf
@@ -3,10 +3,10 @@ resource "aws_security_group" "this" {
   vpc_id = var.region.vpc.vpc_id
 
   ingress {
-    from_port = var.database_port
-    to_port   = var.database_port
-    protocol  = "TCP"
-    self      = true
+    from_port   = var.database_port
+    to_port     = var.database_port
+    protocol    = "TCP"
+    cidr_blocks = ["10.0.0.0/8"]
   }
 
   tags = {
