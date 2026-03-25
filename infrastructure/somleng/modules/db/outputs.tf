80b41b6208d1182f29cba43c58fd8109b3fcd26b
David Wilkie <dwilkie@gmail.com>
2026-03-25 18:28:55 +0700

    Add cross-region sgs (#118)

--- infrastructure/somleng/modules/db/outputs.tf
+++ infrastructure/somleng/modules/db/outputs.tf
@@ -6,6 +6,10 @@ output "security_group" {
   value = aws_security_group.this
 }
 
+output "cross_region_security_group" {
+  value = aws_security_group.cross_region
+}
+
 output "master_password_parameter" {
   value     = aws_ssm_parameter.db_master_password
   sensitive = true
--- infrastructure/somleng/modules/db/sg.tf
+++ infrastructure/somleng/modules/db/sg.tf
@@ -13,3 +13,19 @@ resource "aws_security_group" "this" {
     Name = "aurora-${local.database_identifier}"
   }
 }
+
+resource "aws_security_group" "cross_region" {
+  name   = "${var.identifier}-cross-region"
+  vpc_id = var.region.vpc.vpc_id
+
+  ingress {
+    from_port   = var.database_port
+    to_port     = var.database_port
+    protocol    = "TCP"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+
+  tags = {
+    Name = "aurora-${local.database_identifier}-cross-region"
+  }
+}
